c232d0f6a041e241bdaace39ac393956ff099e92
Maurice Ekkel <67467165+Maurice852@users.noreply.github.com>
2020-11-30 22:45:17 +0100

    Add files via upload

---
+++ Quick sort-dom 2/Quick sort-dom 2/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quick_sort_dom_2
+{
+    /// <summary>
+    /// implement a recursive sort algo
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int n = 10, min = 0, max = 20;
+
+        
+        /// <summary>
+        /// get random input and sort by algo
+        /// and show output
+        /// </summary>
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            int[] a = Quicksort.FillArrayRandom(n, min, max);
+            for (int i = 0; i < a.Length; i++) listBox1.Items.Add(a[i]);
+            int[] final = Quicksort.SortArray(a);
+            for (int i = 0; i < final.Length; i++) listBox2.Items.Add(final[i]);
+
+        }
+    }
+}
---
+++ Quick sort-dom 2/Quick sort-dom 2/Quicksort.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quick_sort_dom_2
+{
+    class Quicksort
+    {
+        /// <summary>
+        /// fill the array with random numbers
+        /// </summary>
+
+        static Random random = new Random();
+        internal static int[] FillArrayRandom(int n, int min, int max)
+        {
+            int[] z = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                z[i] = random.Next(min, max);
+            }
+            return z;
+        }
+
+        /// <summary>
+        /// sort a asc
+        /// </summary>
+        /// <algo>
+        /// Sort the array by calculating the average,
+        /// count amount of values > average
+        /// then creating 2 arrays for numbers > and <= than the average.
+        /// stop if there are no values > average (all values are equal)
+        /// fill both arrays with correct data
+        /// recurse those 2 arrays
+        /// finally we concat both the arrays to show the final sorted array.
+        /// </algo>
+        internal static int[] SortArray(int[] a)
+        {
+            double average = 0;
+            for (int i = 0; i < a.Length; i++) average += a[i];
+            average /= a.Length;
+            int b = 0;
+            for (int i = 0; i < a.Length; i++) if (a[i] > average) b++;
+            //stopcondition
+            if (b == 0) return a;
+            int kg = a.Length - b;
+            int[] kgar = new int[kg];
+            int[] gar = new int[b];
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] > average) gar[--b] = a[i];
+                else kgar[--kg] = a[i];
+            }
+            kgar = SortArray(kgar);
+            gar = SortArray(gar);
+            for (int i = 0; i < kgar.Length; i++) a[i] = kgar[i];
+            for (int i = 0; i < gar.Length; i++) a[i + kgar.Length] = gar[i];
+            return a;
+        }
+    }
+}
